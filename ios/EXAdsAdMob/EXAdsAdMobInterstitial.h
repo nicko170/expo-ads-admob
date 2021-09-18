7ffca97246277220b0d4cad4d53d5224aca588db
nicko170 <nick@npratley.net>
2021-09-19 09:52:21 +1000

    lol this wont work

---
+++ ios/EXAdsAdMob/EXAdsAdMobInterstitial.h
@@ -0,0 +1,8 @@
+#import <ExpoModulesCore/EXDefines.h>
+#import <ExpoModulesCore/EXEventEmitter.h>
+#import <ExpoModulesCore/EXModuleRegistryConsumer.h>
+
+#import <GoogleMobileAds/GoogleMobileAds.h>
+
+@interface EXAdsAdMobInterstitial : EXExportedModule <EXEventEmitter, EXModuleRegistryConsumer, GADInterstitialDelegate>
+@end
